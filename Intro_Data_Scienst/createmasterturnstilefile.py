85252cdbd98614a89cd66a938c076467e6bd147e
ddrsmiLe <ddrsmile@gmail.com>
2018-06-05 01:47:15 +0900

    removed unnecessary files

--- Intro_Data_Scienst/createmasterturnstilefile.py
+++
@@ -1,7 +0,0 @@
-def cmtf(filenames, output_file):
-  with open(output_file, 'w') as master_file:
-    master_file.write('C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n')
-    for filename in filenames:
-      reader = open(filename, 'r')
-      for row in reader:
-        master_file.write(row)
